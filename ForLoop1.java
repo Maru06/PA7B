4885df93155efdafa369db8972295bc4f49be79a
Maru06 <marix5744@gmail.com>
2018-09-28 22:55:53 -0500

    tabla_multiplicar

--- ForLoop1.java
+++ ForLoop1.java
@@ -1,12 +1,23 @@
+
 import javax.swing.JOptionPane;
 public class ForLoop1{
   public static void main(String[] args) {
+    int quest = 0;
+    do{
+    int tabla = Integer.parseInt(JOptionPane.showInputDialog("Ingresa que tabla quieres ver:"));
+    String salida = "";
 
-//inicializacion variable, condicion, incremento
-    for(int i = 0; i <= 9; i++ ){
-    int tabla = Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese un numero"));
-    String salida += (tabla + " x " + (i + 1) + " = " + (i + 1)* tabla + "\n");
-  }while (salida !=0 );
+    for (int i = 0; i <= 10  ; i++ ) {
+
+      //JOptionPane.showMessageDialog(null,"1 x" + i + "=" + m);
+
+      salida += (tabla + " x " + (i + 1) + " = " + (i + 1)* tabla + "\n");
+    }
     JOptionPane.showMessageDialog(null, salida);
+    quest = Integer.parseInt(JOptionPane.showInputDialog("¿Deseas buscar otra tabla? \n"+ "Presiona 0 Si quieres \n" + "Presiona 1 Si NO quieres"));
+    }
+    while (quest == 0);
+
+
   }
 }
