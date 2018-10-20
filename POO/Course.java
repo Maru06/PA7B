a8e9c993d79b61cd8daef049eae1f9a355d14810
Maru06 <marix5744@gmail.com>
2018-10-20 00:32:46 -0500

    19-10-2018

--- POO/Course.java
+++ POO/Course.java
@@ -1,42 +1,66 @@
 public class Course{
-  private String courseName;
-  private String[] students;
-  private int numberOfStudents =0;
+
+private String courseName;
+
+ private String[] students = new String[100];
+
+ private int numberOfStudents = 0;
+
   public Course(){
 
+
   }
-  public Course (String courseName){
-    this.courseName= courseName;
+
+  public Course(String courseName){
+
+   this.courseName = courseName;
   }
-  public String getCourseName(){
+
+
+public String getCourseName(){
     return this.courseName;
   }
-  public void setCourseName(String courseName){
-    this.courseName =courseName;
-  }
-  public void addStudent(String student){
-    this.students[numberOfStudents]=student;
-    this.numberOfStudents++;
-  }
-  public void dropStudents(String student){
-    for (int i = 0; i < numberOfStudents; i++) {
-	      if (students[i].equalsIgnoreCase(student)) {
-	        // Move students[i + 1] to students[i], etc.
-	        for (int k = i + 1; k < numberOfStudents; k++) {
-	          students[k - 1] = students[k];
-	        }
 
-	        numberOfStudents--;
-	        break;
-	      }
-	    }
 
+ public void setCourseName(String courseName){
+
+  this.courseName = courseName;
+
+ }
+
+
+public void addStudent(String student){
+
+  this.students[numberOfStudents] = student;
+
+   this.numberOfStudents++;
+
+}
+
+
+ public void dropStudent(String student){
+   for (int i = 0; i < numberOfStudents; i++) {
+ 	      if (students[i].equalsIgnoreCase(student)) {
+
+ 	        for (int k = i + 1; k < numberOfStudents; k++) {
+ 	          students[k - 1] = students[k];
+ 	        }
+
+ 	        numberOfStudents--;
+ 	        break;
+ 	      }
+ 	    }
+    //tarea para ustedes
   }
-  public String[]getStudents(){
+
+  public String[] getStudents(){
+
     return this.students;
   }
-  public int getNumberOfstudents(){
-    return this.numberOfStudents;
+
+  public int getNumberOfStudents(){
+
+ return this.numberOfStudents;
   }
 
-  }
+}
--- POO/CourseImpl.java
+++ POO/CourseImpl.java
@@ -1,79 +1,95 @@
 import javax.swing.JOptionPane;
+
 public class CourseImpl{
+
 public static void main(String[] args) {
-/*  int opc = 0;
-    do{
+
+  int opc = 0;
+     Course curso1 = new Course();
+      do {
+        opc= Integer.parseInt(JOptionPane.showInputDialog(null,"Seleccione la opcion a realizar \n 1.-Crear Curso \n 2.-Agregar Estudiantes \n 3.-Eliminar Estudiantes \n 4.-Mostrar Estudiantes \n 5.-Mostrar Numero de Estudiantes \n 6.- Salir"));
+        switch(opc){
+          case 1:
+          //crear curso
+            curso1.SetCourseName(JOptionPane.showInputDialog(null,"Ingrese nombre del curso"));
+          break;
+          case 2:
+          //agregar estudiante
+            curso1.addStudent(JOptionPane.showInputDialog(null,"ingrese nombre del estudiante"));
+
+          break;
+          case 3:
+          //eliminar
+            curso1.dropStudent("");
+          break;
+          case 4:
+          //mostrar estudiantes
+          String [] studentsDeCurso1= curso1.getStudents();
+          String students = "";
+          for (int i = 0;i < curso1.getNumberOfStudents() ;i ++ ) {
+            students += studentsDeCurso1[i]+ "\n";
+          }
+          JOptionPane.showMessageDialog(null, students);
+          break;
+          case 5:
+          //mostrar numero de estudiantes
+          JOptionPane.showMessageDialog(null,curso1.getNumberOfStudents());
+          break;
+          default:
+          //mensaje de opcion
+        }
+      } while (opc!=6);
 
 
-     switch(opc){
 
- case 1:
-        //crear curso
+/*
 
- break;
+Course curso1 = new Course();
 
-       case 2:
-    //agregar estudiantes
+ // curso1.courseName = "Inglés";
 
-  break;
+  curso1.setCourseName("Inglés");
 
 
-      case 3:
-        //eliminar estudiantes
+ curso1.addStudent("Luis Alberto");
 
-      break;
+  curso1.addStudent("Maria");
 
- case 4:
-        // mostrar estudiantes
-
-     break;
-
-     case 5:
-
-    //mostrar numero de estudiantes
-
- break;
+ curso1.addStudent("Lucero");
 
 
-     default:
-        //Mensaje de eliga opcion correcta
+   String [] studentsDeIngles = curso1.getStudents();
 
-  }
+   String students = "";
+
+ for (int i = 0; i < curso1.getNumberOfStudents() ; i ++) {
+
+      students += studentsDeIngles[i]+ "\n";
 
  }
-while(true);*/
+
+
+   JOptionPane.showMessageDialog(null, students);
+
+
+  Course curso2 = new Course();
+
+curso2.setCourseName("Mate");
+
+
+curso2.addStudent("Brandon");
+
+  curso2.addStudent("Angel");
+
+   curso2.addStudent("Steven");
+
+
+   JOptionPane.showMessageDialog(null, curso1.getCourseName());
+
+
+   JOptionPane.showMessageDialog(null, curso2.getCourseName());*/
 
 
 
-   Course course3=new Course("programacion");
-   course3.addStudent("agustin");
-
-
- Course curso1=new Course();
- //curso1.courseName="ingles";
- curso1.setCourseName("ingles");
- curso1.addStudent("luis alberto");
- curso1.addStudent("maria");
- curso1.addStudent("lucero");
- String []studentsDeIngles =curso1.getStudents();
- String students="";
- for (int i=0; i<curso1.getNumberOfstudents();i++){
-   students+= studentsDeIngles[i]+"\n";
  }
-
-
-
- JOptionPane.showMessageDialog(null,students);
- Course curso2=new Course();
- curso2.setCourseName("mate");
-  curso2.addStudent("brandon");
-   curso2.addStudent("angel");
-   curso2.addStudent("steven");
- JOptionPane.showMessageDialog(null,curso1.getCourseName());
- JOptionPane.showMessageDialog(null,curso2.getCourseName());
-
-
-
-}
-
 }
